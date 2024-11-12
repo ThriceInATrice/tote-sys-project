ae4ba0f98cbb5718ebabc95aad409d6aaaf496f3
Grace <graceharnup@gmail.com>
2024-11-12 13:56:57 +0000

    add exception to get_new_data_from_database

--- src/get_new_data_from_database.py
+++ src/get_new_data_from_database.py
@@ -7,25 +7,26 @@ def get_new_data_from_database(credentials_id, last_update=None):
     timeframe_string = ""
     if last_update:
         timeframe_string = f"WHERE last_updated BETWEEN {last_update} AND {now}"
+    try:
+        updated_data = {now: []}
+        tables = get_tables(credentials_id)
+        print(f"there are {len(tables)} and they are {tables}")
+        for table in tables:
+            if table != "_prisma_migrations":
+                cursor = connect_to_db(credentials_id)
+                cursor.execute(
+                    f"""
+                    SELECT *
+                    FROM {table}
+                    {timeframe_string}
+                """
+                )
+                results = cursor.fetchall()
+                updated_data[now].append(results)
 
-    updated_data = {now: []}
-    tables = get_tables(credentials_id)
-    print(f"there are {len(tables)} and they are {tables}")
-    for table in tables:
-        if table != "_prisma_migrations":
-            cursor = connect_to_db(credentials_id)
-            cursor.execute(
-                f"""
-                SELECT *
-                FROM {table}
-                {timeframe_string}
-            """
-            )
-            results = cursor.fetchall()
-            updated_data[now].append(results)
-
-    return updated_data
-
+        return updated_data
+    except Exception as e:
+        print("Database connection failed due to {}".format(e))
 
 def get_tables(credentials_id):
     table_query = """
