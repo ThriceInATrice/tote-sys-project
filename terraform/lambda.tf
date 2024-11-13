add1bf0f845f2caf6b0fa0b45c28f1fe8a780e8c
Joanna Link <jt3link@gmail.com>
2024-11-13 09:23:50 +0000

    still working on layer

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -1,13 +1,13 @@
 resource "aws_lambda_function" "ingestion_lambda" {
   function_name = "ingestion_lambda"
   s3_bucket = aws_s3_bucket.code_bucket.bucket
-  s3_key = "ingestion_lambda/function.zip"
+  s3_key = "${var.extract_lambda}/function.zip"
   source_code_hash = data.archive_file.extract_lambda.output_base64sha256
   role = aws_iam_role.lambda_role.arn
   handler = "ingestion_lambda.lambda_handler"
   runtime = "python3.12"
   timeout = 10
-  depends_on = [ aws_s3_object.lambda_code ]
+  depends_on = [ aws_s3_object.lambda_code, aws_s3_object.layer_code ]
 }
 
 data "archive_file" "extract_lambda" {
@@ -18,13 +18,16 @@ data "archive_file" "extract_lambda" {
 
 data "archive_file" "layer" {
     type = "zip"
-    source_dir = "${path.module}/../src/lambda_layer/layer_content/"
+    source_dir = "${path.module}/layer_files/"
     output_path = "${path.module}/../packages/layer_content.zip"
 }
 
 resource "aws_lambda_layer_version" "psycopg2_layer" {
+  s3_bucket = aws_s3_bucket.code_bucket.bucket
+  # s3_bucket = "code-bucket20241112164137527200000004"
+  s3_key = "${var.extract_lambda}/layer_content.zip"
   layer_name          = "psycopg2_layer"
   compatible_runtimes = [var.python_runtime]
-  filename = "${path.module}/../packages/layer_content.zip"
+  # filename = "${path.module}/../packages/layer_content.zip"
 }
 
