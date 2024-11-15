d31a06a8e80c2a69f9fd6c888b59d666c8f5f2dd
Joanna Link <jt3link@gmail.com>
2024-11-15 11:00:38 +0000

    added extraction times bucket permissions to lambda

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -87,3 +87,20 @@ resource "aws_iam_policy" "lambda_secrets_manager" {
     role = aws_iam_role.lambda_role.name
     policy_arn = aws_iam_policy.lambda_secrets_manager.arn
   }
+
+  data "aws_iam_policy_document" "s3_extraction_times_document" {
+    statement {
+      actions = ["s3:PutObject"]
+      resources = ["arn:aws:s3:::extraction-times-20241111134946737900000001/*"]
+    }
+}
+
+resource "aws_iam_policy" "extraction_times_policy" {
+  name_prefix = "s3-extraction-times-lambda-policy"
+  policy = data.aws_iam_policy_document.s3_extraction_times_document.json
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_s3__extraction_policy_attachment" {
+  role = aws_iam_role.lambda_role.name
+  policy_arn = aws_iam_policy.extraction_times_policy.arn
+}
