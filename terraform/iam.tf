5c10d4e60daa9452da2027fa550646c0e165b62a
iqrafar <iqrafarooq1944@gmail.com>
2024-11-15 15:09:13 +0000

    cloudwatch logs fixed and logging in lambda handler

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -1,5 +1,5 @@
 resource "aws_iam_role" "lambda_role" {
-  name_prefix = "extract-lambda-role-"
+  name_prefix        = "extract-lambda-role-"
   assume_role_policy = <<EOF
 
     {
@@ -23,44 +23,48 @@ resource "aws_iam_role" "lambda_role" {
 
 
 data "aws_iam_policy_document" "s3_document" {
-    statement {
-      actions = ["s3:PutObject"]
-      resources = ["arn:aws:s3:::ingestion-bucket-20241111133940921900000001/*"]
-    }
-}
-
-data "aws_iam_policy_document" "cw_document" {
-    statement {
-      actions = ["logs:CreateLogGroup"]
-      resources = ["arn:aws:logs:${data.aws_region.current.name}:${data.aws_region.current.id}:*"]
-      }
-    statement {
-    effect = "Allow"
-    actions = ["logs:CreateLogStream", "logs:PutLogEvents"]
-    resources = ["arn:aws:logs:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:/aws/lambda/${var.extract_lambda}:*"]
+  statement {
+    actions   = ["s3:PutObject"]
+    resources = ["arn:aws:s3:::ingestion-bucket-20241111133940921900000001/*"]
   }
 }
 
+data "aws_iam_policy_document" "cw_document" {
+  statement {
+    effect = "Allow"
+    actions   = ["logs:CreateLogGroup"]
+    resources = ["arn:aws:logs:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:log-group:/aws/lambda/*"]
+  }
+  statement {
+    effect    = "Allow"
+    actions   = ["logs:CreateLogStream", "logs:PutLogEvents"]
+    resources = ["arn:aws:logs:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:log-group:/aws/lambda/*"]
+  }
+}
+# cloudwatch logging resources 
+# creates a log group and a log stream for each deployment within log group
+# give permission to create Log Groups in your account
+# give permission to create Log Streams and put Log Events in the lambda's own Log Group
 
 
 resource "aws_iam_policy" "s3_policy" {
   name_prefix = "s3-ingestion-lambda-policy"
-  policy = data.aws_iam_policy_document.s3_document.json
+  policy      = data.aws_iam_policy_document.s3_document.json
 }
 
 resource "aws_iam_policy" "cw_policy" {
   name_prefix = "cw-ingestion-lambda-policy"
-  policy = data.aws_iam_policy_document.cw_document.json
+  policy      = data.aws_iam_policy_document.cw_document.json
 }
 
 
 resource "aws_iam_role_policy_attachment" "lambda_s3_policy_attachment" {
-  role = aws_iam_role.lambda_role.name
+  role       = aws_iam_role.lambda_role.name
   policy_arn = aws_iam_policy.s3_policy.arn
 }
 
 resource "aws_iam_role_policy_attachment" "lambda_cw_policy_attachment" {
-  role = aws_iam_role.lambda_role.name
+  role       = aws_iam_role.lambda_role.name
   policy_arn = aws_iam_policy.cw_policy.arn
 }
 
@@ -68,28 +72,28 @@ resource "aws_iam_policy" "lambda_secrets_manager" {
   name = "lambda_secrets_manager"
   # role = aws_iam_role.lambda_role.id
   policy = jsonencode({
-        "Version": "2012-10-17",
-        "Statement": [
-            {
-                "Effect": "Allow",
-                "Action": [
-                    "secretsmanager:GetSecretValue"
-                  ],
-                # "Resource": "arn:aws:secretsmanager:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}totesys-db-creds*"
-                # "Resource": "arn:aws:secretsmanager:eu-west-2:881490134104:totesys-db-creds-Q5tZCs"
-                "Resource": "*"
-            }
-          ]
-        })
-  }
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Effect" : "Allow",
+        "Action" : [
+          "secretsmanager:GetSecretValue"
+        ],
+        # "Resource": "arn:aws:secretsmanager:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}totesys-db-creds*"
+        # "Resource": "arn:aws:secretsmanager:eu-west-2:881490134104:totesys-db-creds-Q5tZCs"
+        "Resource" : "*"
+      }
+    ]
+  })
+}
 
-  resource "aws_iam_role_policy_attachment" "lambda_secret_attachment" {
-    role = aws_iam_role.lambda_role.name
-    policy_arn = aws_iam_policy.lambda_secrets_manager.arn
-  }
+resource "aws_iam_role_policy_attachment" "lambda_secret_attachment" {
+  role       = aws_iam_role.lambda_role.name
+  policy_arn = aws_iam_policy.lambda_secrets_manager.arn
+}
 
-  data "aws_iam_policy_document" "s3_extraction_times_document" {
-    statement {
+data "aws_iam_policy_document" "s3_extraction_times_document" {
+  statement {
       actions = ["s3:PutObject"]
       resources = ["arn:aws:s3:::extraction-times-20241111134946737900000001/*"]
     }
