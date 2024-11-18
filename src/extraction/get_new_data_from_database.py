738a0dcb9f72cc008f2d860b851f08a9ffc0449d
Joanna Link <jt3link@gmail.com>
2024-11-18 13:46:19 +0000

    add logging to get_new_data

--- src/extraction/get_new_data_from_database.py
+++ src/extraction/get_new_data_from_database.py
@@ -40,7 +40,11 @@ def get_new_data_from_database(credentials_id, last_extraction=None):
                     {timeframe_string}
                 """
                 )
-
+                logger.info(f"""
+                    SELECT *
+                    FROM {table}
+                    {timeframe_string}
+                """)
                 results = cursor.fetchall()
                 column_names = [desc[0] for desc in cursor.description]
                 new_data[now].append(
