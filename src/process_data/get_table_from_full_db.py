c2e2fe8070f3ea6b7995e7e3dde9e4f443851edb
L S <ls@louis-mac.lan>
2024-11-20 14:28:03 +0000

    removed stuff

--- src/process_data/get_table_from_full_db.py
+++ src/process_data/get_table_from_full_db.py
@@ -2,13 +2,13 @@ import boto3
 import json
 
 def get_table_from_full_db(table_name):
-    bucket_name = 'ingestion-bucket-20241111133940921900000001'
-    key_name = '2024/11/18/2024-11-18 17:13:38.045778.json'
+    bucket_name = ''
+    key_name = ''
 
     client = boto3.client('s3')
     response = client.get_object(Bucket=bucket_name, Key=key_name)
 
     data = json.loads(response['Body'].read())
     table = data['data'][table_name]
-    
+
     return table
