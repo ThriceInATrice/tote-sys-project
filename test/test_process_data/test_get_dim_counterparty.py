ba2a27901e16b9653f2c3611f0a27cef94349767
L S <ls@louis-mac.lan>
2024-11-21 09:27:54 +0000

    fixed tests for get_dim_counterparty

--- test/test_process_data/test_get_dim_counterparty.py
+++ test/test_process_data/test_get_dim_counterparty.py
@@ -1,5 +1,9 @@
+import os
 import json
 from src.process_data.get_dim_counterparty import get_dim_counterparty
+
+DB_CREDENTIALS_ID = os.getenv("DB_CREDENTIALS_ID")
+
 
 with open('test/test_process_data/test_data.json', 'r', encoding='utf-8') as f:
     data = json.load(f)['data']
@@ -8,12 +12,13 @@ test_counterparty = data['counterparty']
 test_address = data['address']
 
 def test_func_returns_list():
-    assert isinstance(get_dim_counterparty(test_counterparty, test_address), list)
+    print(DB_CREDENTIALS_ID)
+    assert isinstance(get_dim_counterparty(DB_CREDENTIALS_ID, test_counterparty), list)
 
 def test_list_contains_items():
-    result = get_dim_counterparty(test_counterparty, test_address)
+    result = get_dim_counterparty(DB_CREDENTIALS_ID, test_counterparty)
     assert len(result) > 0
 
 def test_list_contains_dict():
-    result = get_dim_counterparty(test_counterparty, test_address)
+    result = get_dim_counterparty(DB_CREDENTIALS_ID, test_counterparty)
     assert all([isinstance(item, dict) for item in result])
